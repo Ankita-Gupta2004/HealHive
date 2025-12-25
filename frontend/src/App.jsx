31b9ff90d1393b9939811127d2f00c7bd632d024
Ankita Gupta <ankitagupta94161@gmail.com>
2025-12-25 16:56:35 +0530

    added frontend folder + tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-amber-300"> hello i am Ankita Gupta</h1>
+    </>
+  )
+}
+
+export default App
