286909ebc3baf55a35302be85a46a3c0f1a9049a
Ankita Gupta <ankitagupta94161@gmail.com>
2025-12-25 19:22:12 +0530

    added hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Hero from './Homepage/Hero';
 import Navbar from './Homepage/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
