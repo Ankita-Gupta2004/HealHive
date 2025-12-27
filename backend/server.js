d6ee1855a5c270dd047a3abc9e72a08e75b904ce
DEEPANSHU8708 <deepanshujindal57@gmail.com>
2025-12-27 17:52:43 +0530

    resolve server.js

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,10 @@ app.use("/api/patient", patientRoutes);
 app.use("/api/payments", paymentRoutes);
 
 
+app.use("/api/doctor", doctorRoutes);
+
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
